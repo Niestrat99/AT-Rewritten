d6f014a6d81e9e018e126d771497d63b25b108cf
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-07-12 16:08:09 +0100

    Added /tpo and /tpohere

---
+++ src/io/github/at/commands/teleport/TpoHere.java
@@ -0,0 +1,48 @@
+package io.github.at.commands.teleport;
+
+import io.github.at.config.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TpoHere implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (Config.featTP()) {
+            if (sender.hasPermission("tbh.tp.admin.tpohere")){
+                if (sender instanceof Player){
+                    Player player = (Player)sender;
+                    if (args.length>0){
+                        if (args[0].equalsIgnoreCase(player.getName())){
+                            sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR: " + ChatColor.RED + "You cannot teleport to yourself!");
+                            return false;
+                        }
+                        Player target = Bukkit.getPlayer(args[0]);
+                        if (target == null){
+                            sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " Either the player is currently offline or doesn't exist.");
+                            return false;
+                        } else {
+                            sender.sendMessage(ChatColor.GREEN + "Teleporting " + ChatColor.YELLOW + target.getName() + ChatColor.GREEN + " to you!");
+                            target.sendMessage(ChatColor.GREEN + "You have been teleported to " + ChatColor.YELLOW + sender.getName() + ChatColor.GREEN + "!");
+                            target.teleport(player);
+                            return false;
+                        }
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " You must include a player name!");
+                        return false;
+                    }
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " You do not have permission to use this command!");
+                return false;
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR: " + ChatColor.RED + "The feature " + ChatColor.GOLD + "Teleport " + ChatColor.RED + "is disabled!");
+            return false;
+        }
+        return false;
+    }
+}
