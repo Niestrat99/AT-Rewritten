7022b86e75b8d3b9ae7bae6f07c26839228b36cd
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-07-12 16:22:33 +0100

    Allowing access to AR/TT utils (also camel case)

--- src/io/github/at/utilities/AcceptRequest.java
+++ src/io/github/at/utilities/AcceptRequest.java
@@ -11,29 +11,29 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class AcceptRequest {
 
-    private void acceptRequest(TPRequest Request) {
-        Player player = Request.getResponder();
-        Request.getRequester().sendMessage(ChatColor.YELLOW + "" + player.getName() + ChatColor.GREEN + " has accepted your teleport Request!");
+    public static void acceptRequest(TPRequest request) {
+        Player player = request.getResponder();
+        request.getRequester().sendMessage(ChatColor.YELLOW + "" + player.getName() + ChatColor.GREEN + " has accepted your teleport Request!");
         player.sendMessage(ChatColor.GREEN + "You've accepted the teleport Request!");
-        if (Request.getType() == TPRequest.TeleportType.TPA_HERE) {
+        if (request.getType() == TPRequest.TeleportType.TPA_HERE) {
             BukkitRunnable movementtimer = new BukkitRunnable() {
                 @Override
                 public void run() {
-                    player.teleport(Request.getRequester());
+                    player.teleport(request.getRequester());
                     MovementManager.getMovement().remove(player);
                     player.sendMessage(CustomMessages.getString("Teleport.eventTeleport"));
                     if (Config.EXPPayment()) {
-                        if (Request.getRequester().getLevel()>Config.EXPTeleportPrice()){
-                            int currentLevel = Request.getRequester().getLevel();
-                            Request.getRequester().setLevel(currentLevel - Config.EXPTeleportPrice());
-                            Request.getRequester().sendMessage(ChatColor.GREEN + "You have paid " + ChatColor.AQUA + Config.EXPTeleportPrice() + ChatColor.GREEN + " EXP Levels for your teleportation Request. You now have " + ChatColor.AQUA + Request.getRequester().getLevel() + ChatColor.GREEN + " EXP Levels!");
+                        if (request.getRequester().getLevel()>Config.EXPTeleportPrice()){
+                            int currentLevel = request.getRequester().getLevel();
+                            request.getRequester().setLevel(currentLevel - Config.EXPTeleportPrice());
+                            request.getRequester().sendMessage(ChatColor.GREEN + "You have paid " + ChatColor.AQUA + Config.EXPTeleportPrice() + ChatColor.GREEN + " EXP Levels for your teleportation Request. You now have " + ChatColor.AQUA + request.getRequester().getLevel() + ChatColor.GREEN + " EXP Levels!");
                         }
                     }
                     if  (Main.getVault() != null && Config.useVault()) {
-                        if (Main.getVault().getBalance(Request.getRequester())>Config.teleportPrice()){
-                            EconomyResponse payment = Main.getVault().withdrawPlayer(Request.getRequester() , Config.teleportPrice());
+                        if (Main.getVault().getBalance(request.getRequester())>Config.teleportPrice()){
+                            EconomyResponse payment = Main.getVault().withdrawPlayer(request.getRequester() , Config.teleportPrice());
                             if (payment.transactionSuccess()){
-                                Request.getRequester().sendMessage(ChatColor.GREEN + "You have paid $" + ChatColor.AQUA + Config.teleportPrice() + ChatColor.GREEN + " for your teleportation Request. You now have $" + ChatColor.AQUA + Main.getVault().getBalance(Request.getRequester()) + ChatColor.GREEN + "!");
+                                request.getRequester().sendMessage(ChatColor.GREEN + "You have paid $" + ChatColor.AQUA + Config.teleportPrice() + ChatColor.GREEN + " for your teleportation Request. You now have $" + ChatColor.AQUA + Main.getVault().getBalance(request.getRequester()) + ChatColor.GREEN + "!");
                             }
                         }
                     }
@@ -47,23 +47,23 @@ public class AcceptRequest {
             BukkitRunnable movementtimer = new BukkitRunnable() {
                 @Override
                 public void run() {
-                    Request.getRequester().teleport(player);
-                    MovementManager.getMovement().remove(Request.getRequester());
-                    Request.getRequester().sendMessage(CustomMessages.getString("Teleport.eventTeleport"));
+                    request.getRequester().teleport(player);
+                    MovementManager.getMovement().remove(request.getRequester());
+                    request.getRequester().sendMessage(CustomMessages.getString("Teleport.eventTeleport"));
                     if (Config.EXPPayment()) {
-                        if (Request.getRequester().getLevel()>Config.EXPTeleportPrice()){
-                            int currentLevel = Request.getRequester().getLevel();
-                            Request.getRequester().setLevel(currentLevel - Config.EXPTeleportPrice());
-                            Request.getRequester().sendMessage(ChatColor.GREEN + "You have paid " + ChatColor.AQUA + Config.EXPTeleportPrice() + ChatColor.GREEN + " EXP Levels for your teleportation Request. You now have " + ChatColor.AQUA + Request.getRequester().getLevel() + ChatColor.GREEN + " EXP Levels!");
+                        if (request.getRequester().getLevel()>Config.EXPTeleportPrice()){
+                            int currentLevel = request.getRequester().getLevel();
+                            request.getRequester().setLevel(currentLevel - Config.EXPTeleportPrice());
+                            request.getRequester().sendMessage(ChatColor.GREEN + "You have paid " + ChatColor.AQUA + Config.EXPTeleportPrice() + ChatColor.GREEN + " EXP Levels for your teleportation Request. You now have " + ChatColor.AQUA + request.getRequester().getLevel() + ChatColor.GREEN + " EXP Levels!");
                         }
                     }
                     if  (Main.getVault() != null && Config.useVault()) {
-                        if (Main.getVault().getBalance(Request.getRequester())>=Config.teleportPrice()){
-                            EconomyResponse payment = Main.getVault().withdrawPlayer(Request.getRequester() , Config.teleportPrice());
+                        if (Main.getVault().getBalance(request.getRequester())>=Config.teleportPrice()){
+                            EconomyResponse payment = Main.getVault().withdrawPlayer(request.getRequester() , Config.teleportPrice());
                             if (payment.transactionSuccess()){
-                                Request.getRequester().sendMessage(ChatColor.GREEN + "You have paid $" + ChatColor.AQUA + Config.teleportPrice() + ChatColor.GREEN + " for your teleportation Request. You now have $" + ChatColor.AQUA + Main.getVault().getBalance(Request.getRequester()) + ChatColor.GREEN + "!");
+                                request.getRequester().sendMessage(ChatColor.GREEN + "You have paid $" + ChatColor.AQUA + Config.teleportPrice() + ChatColor.GREEN + " for your teleportation Request. You now have $" + ChatColor.AQUA + Main.getVault().getBalance(request.getRequester()) + ChatColor.GREEN + "!");
                             } else {
-                                Request.getRequester().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR: " + ChatColor.RED + payment.errorMessage);
+                                request.getRequester().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR: " + ChatColor.RED + payment.errorMessage);
                             }
                         }
 
@@ -71,11 +71,11 @@ public class AcceptRequest {
                     }
                 }
             };
-            MovementManager.getMovement().put(Request.getRequester(), movementtimer);
+            MovementManager.getMovement().put(request.getRequester(), movementtimer);
             movementtimer.runTaskLater(Main.getInstance(), Config.teleportTimer()*20);
-            Request.getRequester().sendMessage(CustomMessages.getString("Teleport.eventBeforeTP").replaceAll("\\{countdown}" , String.valueOf(Config.teleportTimer())));
+            request.getRequester().sendMessage(CustomMessages.getString("Teleport.eventBeforeTP").replaceAll("\\{countdown}" , String.valueOf(Config.teleportTimer())));
         }
-        Request.destroy();
+        request.destroy();
     }
 
 }
--- src/io/github/at/utilities/TeleportTests.java
+++ src/io/github/at/utilities/TeleportTests.java
@@ -7,21 +7,26 @@ import org.bukkit.entity.Player;
 
 public class TeleportTests {
 
-    private boolean teleportTests(Player player, String[] args, String type) {
+    public static boolean teleportTests(Player player, String[] args, String type) {
+
         // Checks if any players have sent a request at all.
-        if (!TeleportRequest.getRequests(player).isEmpty()) {
+        if (!TPRequest.getRequests(player).isEmpty()) {
+
             // Checks if there's more than one request.
-            if (TeleportRequest.getRequests(player).size() > 1) {
+            if (TPRequest.getRequests(player).size() > 1) {
+
                 // If the player has specified the request they're accepting.
                 if (args.length > 0) {
+
                     // Get the player.
                     Player target = Bukkit.getPlayer(args[0]);
                     if (target == null) {
                         player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " Either the player is currently offline or doesn't exist.");
                         return false;
                     } else {
+
                         // Get the request that was sent by the target.
-                        TeleportRequest request = TeleportRequest.getRequestByReqAndResponder(player, target);
+                        TPRequest request = TPRequest.getRequestByReqAndResponder(player, target);
                         if (request == null) {
                             player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + "You don't have any pending requests from " + ChatColor.YELLOW + target.getName() + ChatColor.RED + "!");
                             return false;
@@ -32,9 +37,9 @@ public class TeleportTests {
                     }
                 } else {
                     // This utility helps in splitting lists into separate pages, like when you list your plots with PlotMe/PlotSquared.
-                    PagedLists<TeleportRequest> requests = new PagedLists<>(TeleportRequest.getRequests(player), 8);
+                    PagedLists<TPRequest> requests = new PagedLists<>(TPRequest.getRequests(player), 8);
                     player.sendMessage(ChatColor.GREEN + "You have multiple teleport requests pending! Click one of the following to " + (type.equalsIgnoreCase("tpayes") ? "accept" : "deny") + ":");
-                    for (TeleportRequest request : requests.getContentsInPage(1)) {
+                    for (TPRequest request : requests.getContentsInPage(1)) {
                         new FancyMessage()
                                 .command("/" + type + " " + request.getRequester().getName())
                                 .color(ChatColor.AQUA)
