d75a74a21678339fcad8f8f225d5360dd0996240
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-07-18 07:45:38 +0100

    Almost forgot TPA and TPAHere (currently empty)

---
+++ src/io/github/at/commands/teleport/TpAll.java
@@ -0,0 +1,12 @@
+package io.github.at.commands.teleport;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TpAll implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        return false;
+    }
+}
