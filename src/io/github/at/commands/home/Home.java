4db4b153e7ce158b6578782ea47d4db26507d6ca
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-07-19 09:16:35 +0100

    Fixed homes placeholder

--- src/io/github/at/commands/home/Home.java
+++ src/io/github/at/commands/home/Home.java
@@ -42,6 +42,29 @@ public class Home implements CommandExecutor {
                                                 return false;
                                             }
 
+                                        } else if (args[1].equalsIgnoreCase("list")) {
+                                            if (sender.hasPermission("at.admin.homes")) {
+                                                StringBuilder hlist = new StringBuilder();
+                                                hlist.append(CustomMessages.getString("Info.homesOther").replaceAll("\\{player}", player.getName()));
+                                                if (Bukkit.getPlayer(args[0]) != null) {
+                                                    try {
+                                                        if (Homes.getHomes(player).size()>0) {
+                                                            for (String home: Homes.getHomes(player).keySet()) {
+                                                                hlist.append(home + ", ");
+                                                            }
+                                                        } else {
+                                                            sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
+                                                            return false;
+                                                        }
+
+                                                    } catch (NullPointerException ex) {
+                                                        sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
+                                                        return false;
+                                                    }
+                                                    sender.sendMessage(hlist.toString());
+                                                    return false;
+                                                }
+                                            }
                                         } else {
                                             sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
                                             return false;
@@ -59,28 +82,48 @@ public class Home implements CommandExecutor {
                                 }
                             }
                         }
-                        try {
-                            if (Homes.getHomes(player).containsKey(args[0])) {
-                                Location location = Homes.getHomes(player).get(args[0]);
-                                teleport(player, location);
-                                return false;
-                            } else if (args[0].equalsIgnoreCase("bed")) {
-                                Location location = player.getBedSpawnLocation();
-                                if (location == null) {
-                                    player.sendMessage(CustomMessages.getString("Error.noBedHome"));
+                        if (PaymentManager.canPay("home", player)) {
+                            try {
+                                if (Homes.getHomes(player).containsKey(args[0])) {
+                                    Location location = Homes.getHomes(player).get(args[0]);
+                                    teleport(player, location, args[0]);
                                     return false;
-                                }
-                                teleport(player, location);
-                                return false;
+                                } else if (args[0].equalsIgnoreCase("bed")) {
+                                    Location location = player.getBedSpawnLocation();
+                                    if (location == null) {
+                                        player.sendMessage(CustomMessages.getString("Error.noBedHome"));
+                                        return false;
+                                    }
+                                    teleport(player, location, args[0]);
+                                    return false;
 
-                            } else {
-                                sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
+                                } else if (args[0].equalsIgnoreCase("list")) {
+                                    StringBuilder hlist = new StringBuilder();
+                                    hlist.append(CustomMessages.getString("Info.homes"));
+                                    try {
+                                        if (Homes.getHomes(player).size()>0){
+                                            for (String home: Homes.getHomes(player).keySet()) {
+                                                hlist.append(home + ", ");
+                                            }
+                                        } else {
+                                            sender.sendMessage(CustomMessages.getString("Error.noHomes"));
+                                            return false;
+                                        }
+                                    } catch (NullPointerException ex) { // If a player has never set any homes
+                                        sender.sendMessage(CustomMessages.getString("Error.noHomes"));
+                                        return false;
+                                    }
+                                    sender.sendMessage(hlist.toString());
+                                } else {
+                                    sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
+                                }
+                            } catch (NullPointerException ex) {
+                                Location location = Homes.getHomes(player).get(args[0]);
+                                teleport(player, location, args[0]);
+                                return false;
                             }
-                        } catch (NullPointerException ex) {
-                            Location location = Homes.getHomes(player).get(args[0]);
-                            teleport(player, location);
-                            return false;
                         }
+
                     } else {
                         sender.sendMessage(CustomMessages.getString("Error.noHomeInput"));
                         return false;
@@ -96,13 +139,13 @@ public class Home implements CommandExecutor {
         return false;
     }
 
-    private void teleport(Player player, Location loc) {
+    private void teleport(Player player, Location loc, String name) {
         if (PaymentManager.canPay("home", player)) {
             if (Config.getTeleportTimer("home") > 0) {
                 BukkitRunnable movementtimer = new BukkitRunnable() {
                     @Override
                     public void run() {
-                        player.sendMessage(CustomMessages.getString("Info.teleportingToHome"));
+                        player.sendMessage(CustomMessages.getString("Info.teleportingToHome").replaceAll("\\{home}",name));
                         player.teleport(loc);
                         MovementManager.getMovement().remove(player);
                         PaymentManager.withdraw("home", player);
@@ -113,7 +156,7 @@ public class Home implements CommandExecutor {
                 player.sendMessage(CustomMessages.getString("Teleport.eventBeforeTP").replaceAll("\\{countdown}", String.valueOf(Config.getTeleportTimer("home"))));
 
             } else {
-                player.sendMessage(CustomMessages.getString("Info.teleportingToHome"));
+                player.sendMessage(CustomMessages.getString("Info.teleportingToHome").replaceAll("\\{home}",name));
                 player.teleport(loc);
                 PaymentManager.withdraw("home", player);
             }
