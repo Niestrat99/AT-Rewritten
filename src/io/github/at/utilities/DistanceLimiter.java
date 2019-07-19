f330257093cfd0e95c2b404f2bc79c80427babd2
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-07-19 18:21:54 +0100

    Distance limits are now ignored when switching worlds

--- src/io/github/at/utilities/DistanceLimiter.java
+++ src/io/github/at/utilities/DistanceLimiter.java
@@ -5,8 +5,9 @@ import org.bukkit.Location;
 
 public class DistanceLimiter {
 
-    public static boolean canTeleport(Location loc1, Location loc2) {
-        if (Config.isDistanceLimiterEnabled()) {
+    public static boolean canTeleport(Location loc1, Location loc2, String command) {
+        if (Config.isDistanceLimiterEnabled() && (command == null || Config.isDistanceLimiterEnabledForCmd(command))) {
+            if (loc1.getWorld() != loc2.getWorld()) return true;
             // ((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1))^0.5
             double distance = Math.pow((Math.pow(loc2.getX() - loc1.getX(), 2)
                     + Math.pow(loc2.getY() - loc1.getY(), 2)
