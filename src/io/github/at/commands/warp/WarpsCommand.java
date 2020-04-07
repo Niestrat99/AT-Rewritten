8a4a63575de89fee4e07d1d5c22e4bdf870a894a
BuildTools <vinjongm@gmail.com>
2020-04-08 03:15:10 +0700

    Added comments

--- src/io/github/at/commands/warp/WarpsCommand.java
+++ src/io/github/at/commands/warp/WarpsCommand.java
@@ -21,10 +21,30 @@ public class WarpsCommand implements CommandExecutor {
             if (commandSender.hasPermission("at.member.warps")){
                 if(Config.isUsingWarpsGUIMenu()){
                         ConfigurationSection warps = Config.getWarpsMenu();
+                        /* Instantiate IconMenu
+                         * e.g: new IconMenu("Your Homes", 9, CoreClass.getInstance()) will create a menu with "Your Homes" as the title and has 9 slots
+                         * The slots have to be multiple of 9
+                         * You have to pass an instance of the plugin on the 3rd parameters for it to work
+                         */
                         IconMenu menu = new IconMenu(CustomMessages.getString("Info.warps"), Config.getWarpsMenuSlots(), CoreClass.getInstance());
                         for (String warpName : warps.getKeys(false)) {
                             ConfigurationSection warp = warps.getConfigurationSection(warpName);
                             if (commandSender.hasPermission("at.member.warp.*") || commandSender.hasPermission("at.member.warp." + warpName) || !warp.getBoolean("hideIfNoPermission")) {
+                                /* Set an option and command to the inventory menu
+                                 * e.g:
+                                 * menu.setOption(11, new ItemStack(Material.GRASS_BLOCK, 1), "RTP", "Teleports you to random location");
+                                 * menu.setCommand(11, "/rtp");
+                                 * will set item inventory on slot 11 with 1x grass block, and rename the item to "RTP" and shows "Teleports you to random location" on the tooltip, and dispatch "/rtp" command on the commandSender
+                                 *
+                                 * You can also do menu.setClickEventHandler() if you want to do more than just a simple command
+                                 * e.g:
+                                 * menu.setClickEventHandler(11, new IconMenu.OptionClickEventHandler() {
+                                 *      @Override
+                                 *      public void onOptionClick(IconMenu.OptionClickEvent event){
+                                 *          System.out.println(event.getPlayer().getDisplayName() + " just clicked something!");
+                                 *      }
+                                 * });
+                                 */
                                 menu.setOption(warp.getInt("slot"), new ItemStack(Material.valueOf(warp.getString("item")), 1), warp.getString("name"), warp.getString("tooltip"));
                                 menu.setCommand(warp.getInt("slot"), "/warp " + warpName);
                             }
