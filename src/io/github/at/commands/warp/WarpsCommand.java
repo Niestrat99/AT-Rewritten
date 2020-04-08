c9e78aa16c505432103223946f5b053143d1ec18
BuildTools <vinjongm@gmail.com>
2020-04-08 14:45:21 +0700

    Fix page index and change comments

--- src/io/github/at/commands/warp/WarpsCommand.java
+++ src/io/github/at/commands/warp/WarpsCommand.java
@@ -22,9 +22,9 @@ public class WarpsCommand implements CommandExecutor {
                 if(Config.isUsingWarpsGUIMenu()){
                         ConfigurationSection warps = Config.getWarpsMenu();
                         /* Instantiate IconMenu
-                         * e.g: new IconMenu("Your Homes", 9, CoreClass.getInstance()) will create a menu with "Your Homes" as the title and has 9 slots
+                         * e.g: new IconMenu("Your Homes", 9, 1, CoreClass.getInstance()) will create a menu with "Your Homes" as the title with 1 page and has 9 slots for each page
                          * The slots have to be multiple of 9
-                         * You have to pass an instance of the plugin on the 3rd parameters for it to work
+                         * You have to pass an instance of the plugin on the 4th parameters for it to work
                          */
                         IconMenu menu = new IconMenu(CustomMessages.getString("Info.warps"), Config.getWarpsMenuSlots(), 1, CoreClass.getInstance());
                         for (String warpName : warps.getKeys(false)) {
@@ -32,9 +32,9 @@ public class WarpsCommand implements CommandExecutor {
                             if (commandSender.hasPermission("at.member.warp.*") || commandSender.hasPermission("at.member.warp." + warpName) || !warp.getBoolean("hideIfNoPermission")) {
                                 /* Set an option and command to the inventory menu
                                  * e.g:
-                                 * menu.setOption(11, new ItemStack(Material.GRASS_BLOCK, 1), "RTP", "Teleports you to random location");
-                                 * menu.setCommand(11, "/rtp");
-                                 * will set item inventory on slot 11 with 1x grass block, and rename the item to "RTP" and shows "Teleports you to random location" on the tooltip, and dispatch "/rtp" command on the commandSender
+                                 * menu.setOption(0, 11, new ItemStack(Material.GRASS_BLOCK, 1), "RTP", "Teleports you to random location");
+                                 * menu.setCommand(0, 11, "/rtp");
+                                 * will set item inventory on page 0 slot 11 with 1x grass block, and rename the item to "RTP" and shows "Teleports you to random location" on the tooltip, and dispatch "/rtp" command on the commandSender
                                  *
                                  * You can also do menu.setClickEventHandler() if you want to do more than just a simple command
                                  * e.g:
@@ -45,8 +45,8 @@ public class WarpsCommand implements CommandExecutor {
                                  *      }
                                  * });
                                  */
-                                menu.setOption(1, warp.getInt("slot"), new ItemStack(Material.valueOf(warp.getString("item")), 1), warp.getString("name"), warp.getString("tooltip"));
-                                menu.setCommand(1, warp.getInt("slot"), "/warp " + warpName);
+                                menu.setOption(0, warp.getInt("slot"), new ItemStack(Material.valueOf(warp.getString("item")), 1), warp.getString("name"), warp.getString("tooltip"));
+                                menu.setCommand(0, warp.getInt("slot"), "/warp " + warpName);
                             }
                         }
                         menu.open((Player)commandSender);
