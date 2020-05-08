00d0b69fff474ab38eb3e46952ce61b5c21c734b
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-05-08 13:56:17 +0100

    what was this, oh no

--- src/io/github/at/utilities/PaymentManager.java
+++ src/io/github/at/utilities/PaymentManager.java
@@ -11,16 +11,18 @@ public class PaymentManager {
 
     // Method used to check if a player can pay for using a command
     public static boolean canPay(String command, Player player) {
-        if (Config.isUsingEXPPayment(command)){
-            if (player.getLevel()<Config.getEXPTeleportPrice(command)){
-                player.sendMessage(CustomMessages.getString("Error.notEnoughEXP").replaceAll("\\{levels}", String.valueOf(Config.getEXPTeleportPrice(command))));
-                return false;
+        if (!player.hasPermission("at.admin.bypass")) {
+            if (Config.isUsingEXPPayment(command)){
+                if (player.getLevel()<Config.getEXPTeleportPrice(command)){
+                    player.sendMessage(CustomMessages.getString("Error.notEnoughEXP").replaceAll("\\{levels}", String.valueOf(Config.getEXPTeleportPrice(command))));
+                    return false;
+                }
             }
-        }
-        if (CoreClass.getVault() != null && Config.isUsingVault(command)) {
-            if (CoreClass.getVault().getBalance(player)<Config.getTeleportPrice(command)){
-                player.sendMessage(CustomMessages.getString("Error.notEnoughMoney").replaceAll("\\{amount}", String.valueOf(Config.getTeleportPrice(command))));
-                return false;
+            if (CoreClass.getVault() != null && Config.isUsingVault(command)) {
+                if (CoreClass.getVault().getBalance(player)<Config.getTeleportPrice(command)){
+                    player.sendMessage(CustomMessages.getString("Error.notEnoughMoney").replaceAll("\\{amount}", String.valueOf(Config.getTeleportPrice(command))));
+                    return false;
+                }
             }
         }
         return true;
