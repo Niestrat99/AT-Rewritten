3d043768283c0c284d6e0d8a2935b4f2834fbce8
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-07-26 19:14:40 +0100

    Handle home tasks async

--- src/io/github/at/commands/home/Home.java
+++ src/io/github/at/commands/home/Home.java
@@ -10,6 +10,7 @@ import io.github.at.utilities.DistanceLimiter;
 import io.github.at.utilities.PaymentManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,109 +26,94 @@ public class Home implements CommandExecutor {
         if (Config.isFeatureEnabled("homes")) {
             if (sender.hasPermission("at.member.home")) {
                 if (sender instanceof Player) {
+                    Bukkit.getScheduler().runTaskAsynchronously(CoreClass.getInstance(), () -> {
+
                     Player player = (Player)sender;
                     String uuid = player.getUniqueId().toString();
                     HashMap<String, Location> homes = Homes.getHomes(uuid);
+                    if (MovementManager.getMovement().containsKey(player.getUniqueId())) {
+                        player.sendMessage(CustomMessages.getString("Error.onCountdown"));
+                        return;
+                    }
+
                     if (args.length>0) {
-                        if (Bukkit.getOfflinePlayer(args[0]) != null) {
-                            if (sender.hasPermission("at.admin.home")) {
+                        if (sender.hasPermission("at.admin.home")) {
+                            OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+                            if (target != null) {
                                 if (args.length > 1) {
-                                    uuid = Bukkit.getOfflinePlayer(args[0]).getUniqueId().toString();
-                                    homes = Homes.getHomes(uuid);
+                                    String uuidOther = target.getUniqueId().toString();
+                                    HashMap<String, Location> homesOther = Homes.getHomes(uuidOther);
                                     try {
-                                        if (homes.containsKey(args[1])) {
-                                            Location tlocation = homes.get(args[1]);
-                                            player.teleport(tlocation);
-                                            sender.sendMessage(CustomMessages.getString("Teleport.teleportingToHomeOther")
-                                                    .replaceAll("\\{player}", Bukkit.getOfflinePlayer(args[0]).getName())
-                                                    .replaceAll("\\{home}", args[1]));
-                                            return true;
-                                        } else if (args[1].equalsIgnoreCase("bed")) {
-                                            Location location = player.getBedSpawnLocation();
-                                            if (location == null) {
-                                                player.sendMessage(CustomMessages.getString("Error.noBedHomeOther").replaceAll("\\{player}", Bukkit.getOfflinePlayer(args[0]).getName()));
-                                                return true;
-                                            }
-
-                                        } else if (args[1].equalsIgnoreCase("list")) {
-                                            if (sender.hasPermission("at.admin.homes")) {
-                                                StringBuilder hlist = new StringBuilder();
-                                                hlist.append(CustomMessages.getString("Info.homesOther").replaceAll("\\{player}", player.getName()));
-                                                if (Bukkit.getPlayer(args[0]) != null) {
-                                                    homes = Homes.getHomes(uuid);
-                                                    try {
-                                                        if (homes.size()>0) {
-                                                            for (String home: homes.keySet()) {
-                                                                hlist.append(home + ", ");
-                                                            }
-                                                            hlist.setLength(hlist.length() - 2);
-                                                        } else {
-                                                            sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
-                                                            return true;
-                                                        }
-
-                                                    } catch (NullPointerException ex) {
-                                                        sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
-                                                        return true;
-                                                    }
-                                                    sender.sendMessage(hlist.toString());
-                                                    return true;
+                                        Location loc;
+                                        switch (args[1].toLowerCase()) {
+                                            case "bed":
+                                                loc = player.getBedSpawnLocation();
+                                                if (loc == null) {
+                                                    player.sendMessage(CustomMessages.getString("Error.noBedHomeOther").replaceAll("\\{player}", args[0]));
+                                                    return;
                                                 }
-                                            }
-                                        } else {
-                                            sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
-                                            return true;
+                                                break;
+                                                case "list":
+                                                    Bukkit.getScheduler().runTask(CoreClass.getInstance(), () -> Bukkit.dispatchCommand(sender, "advancedteleport:homes " + args[0]));
+                                                    return;
+                                                default:
+                                                    if (homesOther.containsKey(args[1])) {
+                                                        loc = homesOther.get(args[1]);
+                                                    } else {
+                                                        sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
+                                                        return;
+                                                    }
                                         }
+                                        Bukkit.getScheduler().runTask(CoreClass.getInstance(), () -> {
+                                            player.teleport(loc);
+                                            sender.sendMessage(CustomMessages.getString("Teleport.teleportingToHomeOther")
+                                                    .replaceAll("\\{player}", args[0])
+                                                    .replaceAll("\\{home}", args[1]));
+                                        });
                                     } catch (NullPointerException ex) {
-                                        Location tlocation = Homes.getHomes(uuid).get(args[1]);
+                                        Location tlocation = Homes.getHomes(uuidOther).get(args[1]);
                                         player.teleport(tlocation);
                                         sender.sendMessage(CustomMessages.getString("Teleport.teleportingToHomeOther")
-                                                .replaceAll("\\{player}", Bukkit.getOfflinePlayer(args[0]).getName().replaceAll("\\{home}", args[1])));
-                                        return true;
+                                                .replaceAll("\\{player}", args[0]).replaceAll("\\{home}", args[1]));
                                     }
+                                    return;
                                 }
                             }
                         }
-                        if (MovementManager.getMovement().containsKey(player.getUniqueId())) {
-                            player.sendMessage(CustomMessages.getString("Error.onCountdown"));
-                            return true;
-                        }
-                        if (PaymentManager.canPay("home", player)) {
-                            try {
-                                if (Homes.getHomes(uuid).containsKey(args[0])) {
-                                    Location location = Homes.getHomes(uuid).get(args[0]);
-                                    teleport(player, location, args[0]);
-                                    return true;
-                                } else if (args[0].equalsIgnoreCase("bed")) {
-                                    Location location = player.getBedSpawnLocation();
-                                    if (location == null) {
-                                        player.sendMessage(CustomMessages.getString("Error.noBedHome"));
-                                        return true;
-                                    }
-                                    teleport(player, location, args[0]);
-                                    return true;
-
-                                } else if (args[0].equalsIgnoreCase("list")) {
-                                    Bukkit.dispatchCommand(sender, "homes");
-                                } else {
-                                    sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
-                                }
-                            } catch (NullPointerException ex) {
-                                Location location = Homes.getHomes(uuid).get(args[0]);
-                                teleport(player, location, args[0]);
-                                return true;
-                            }
-                        }
-
                     } else {
                         if (homes.size() == 1) {
                             String name = homes.keySet().iterator().next();
                             teleport(player, homes.get(name), name);
                         } else {
                             sender.sendMessage(CustomMessages.getString("Error.noHomeInput"));
-                            return true;
+                            return;
                         }
                     }
+
+                    if (PaymentManager.canPay("home", player)) {
+                        try {
+                            if (Homes.getHomes(uuid).containsKey(args[0])) {
+                                Location location = Homes.getHomes(uuid).get(args[0]);
+                                teleport(player, location, args[0]);
+                            } else if (args[0].equalsIgnoreCase("bed")) {
+                                Location location = player.getBedSpawnLocation();
+                                if (location == null) {
+                                    player.sendMessage(CustomMessages.getString("Error.noBedHome"));
+                                    return;
+                                }
+                                teleport(player, location, args[0]);
+
+                            } else if (args[0].equalsIgnoreCase("list")) {
+                                Bukkit.getScheduler().runTask(CoreClass.getInstance(), () -> Bukkit.dispatchCommand(sender, "advancedteleport:homes " + args[0]));
+                            } else {
+                                sender.sendMessage(CustomMessages.getString("Error.noSuchHome"));
+                            }
+                        } catch (NullPointerException ex) {
+                            Location location = Homes.getHomes(uuid).get(args[0]);
+                            teleport(player, location, args[0]);
+                        }
+                    }
+                    });
                 } else {
                     sender.sendMessage(CustomMessages.getString("Error.notAPlayer"));
                 }
@@ -144,29 +130,31 @@ public class Home implements CommandExecutor {
             player.sendMessage(CustomMessages.getString("Error.tooFarAway"));
             return;
         }
-        ATTeleportEvent event = new ATTeleportEvent(player, loc, player.getLocation(), name, ATTeleportEvent.TeleportType.HOME);
-        if (!event.isCancelled()) {
-            if (PaymentManager.canPay("home", player)) {
-                if (Config.getTeleportTimer("home") > 0 && !player.hasPermission("at.admin.bypass.timer")) {
-                    BukkitRunnable movementtimer = new BukkitRunnable() {
-                        @Override
-                        public void run() {
-                            player.sendMessage(CustomMessages.getString("Teleport.teleportingToHome").replaceAll("\\{home}",name));
-                            player.teleport(loc);
-                            MovementManager.getMovement().remove(player.getUniqueId());
-                            PaymentManager.withdraw("home", player);
-                        }
-                    };
-                    MovementManager.getMovement().put(player.getUniqueId(), movementtimer);
-                    movementtimer.runTaskLater(CoreClass.getInstance(), Config.getTeleportTimer("home") * 20);
-                    player.sendMessage(CustomMessages.getEventBeforeTPMessage().replaceAll("\\{countdown}", String.valueOf(Config.getTeleportTimer("home"))));
+        Bukkit.getScheduler().runTask(CoreClass.getInstance(), () -> {
+            ATTeleportEvent event = new ATTeleportEvent(player, loc, player.getLocation(), name, ATTeleportEvent.TeleportType.HOME);
+            if (!event.isCancelled()) {
+                if (PaymentManager.canPay("home", player)) {
+                    if (Config.getTeleportTimer("home") > 0 && !player.hasPermission("at.admin.bypass.timer")) {
+                        BukkitRunnable movementtimer = new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                player.sendMessage(CustomMessages.getString("Teleport.teleportingToHome").replaceAll("\\{home}",name));
+                                player.teleport(loc);
+                                MovementManager.getMovement().remove(player.getUniqueId());
+                                PaymentManager.withdraw("home", player);
+                            }
+                        };
+                        MovementManager.getMovement().put(player.getUniqueId(), movementtimer);
+                        movementtimer.runTaskLater(CoreClass.getInstance(), Config.getTeleportTimer("home") * 20);
+                        player.sendMessage(CustomMessages.getEventBeforeTPMessage().replaceAll("\\{countdown}", String.valueOf(Config.getTeleportTimer("home"))));
 
-                } else {
-                    player.sendMessage(CustomMessages.getString("Teleport.teleportingToHome").replaceAll("\\{home}",name));
-                    player.teleport(loc);
-                    PaymentManager.withdraw("home", player);
+                    } else {
+                        player.sendMessage(CustomMessages.getString("Teleport.teleportingToHome").replaceAll("\\{home}",name));
+                        player.teleport(loc);
+                        PaymentManager.withdraw("home", player);
+                    }
                 }
             }
-        }
+        });
     }
 }
--- src/io/github/at/commands/home/HomesCommand.java
+++ src/io/github/at/commands/home/HomesCommand.java
@@ -16,61 +16,77 @@ public class HomesCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (Config.isFeatureEnabled("homes")) {
             if (sender.hasPermission("at.member.homes")) {
-                if (args.length>0) {
-                    if (sender.hasPermission("at.admin.homes")) {
-                        OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
-                        if (player != null) {
-                            String uuid = player.getUniqueId().toString();
-                            FancyMessage hlist = new FancyMessage();
-                            hlist.text(CustomMessages.getString("Info.homesOther").replaceAll("\\{player}", player.getName()));
-                            try {
-                                if (Homes.getHomes(uuid).size()>0) {
-                                    for (String home : Homes.getHomes(uuid).keySet()) {
-                                        hlist.then(home)
-                                                .command("/home " + args[0] + " " + home)
-                                                .tooltip(CustomMessages.getString("Tooltip.homes").replaceAll("\\{home}", home))
-                                                .then(", ");
+                Bukkit.getScheduler().runTaskAsynchronously(CoreClass.getInstance(), () -> {
+                    if (args.length>0) {
+                        if (sender.hasPermission("at.admin.homes")) {
+                            OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
+                            if (player != null) {
+                                String uuid = player.getUniqueId().toString();
+                                FancyMessage hlist = new FancyMessage();
+                                hlist.text(CustomMessages.getString("Info.homesOther").replaceAll("\\{player}", player.getName()));
+                                try {
+                                    if (Homes.getHomes(uuid).size()>0) {
+                                        for (String home : Homes.getHomes(uuid).keySet()) {
+                                            hlist.then(home)
+                                                    .command("/home " + args[0] + " " + home)
+                                                    .tooltip(CustomMessages.getString("Tooltip.homes").replaceAll("\\{home}", home))
+                                                    .then(", ");
+                                        }
+                                        if (player.getBedSpawnLocation() != null) {
+                                            hlist.then("bed")
+                                                    .command("/home " + args[0] + " bed")
+                                                    .tooltip(CustomMessages.getString("Tooltip.homes").replaceAll("\\{home}", "bed"))
+                                                    .then(", ");
+                                        }
+                                        hlist.text(""); //Removes trailing comma
+                                    } else {
+                                        sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
+                                        return;
                                     }
-                                    hlist.text(""); //Removes trailing comma
-                                } else {
+
+                                } catch (NullPointerException ex) {
                                     sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
-                                    return true;
+                                    return;
                                 }
+                                Bukkit.getScheduler().runTask(CoreClass.getInstance(), () -> hlist.send(sender));
 
-                            } catch (NullPointerException ex) {
-                                sender.sendMessage(CustomMessages.getString("Error.noHomesOther").replaceAll("\\{player}", player.getName()));
-                                return true;
-                            }
-                            hlist.send(sender);
-                            return true;
-                        } // Otherwise we'll just get the main player's homes
-                    }
-                }
-                if (sender instanceof Player) {
-                    Player player = (Player)sender;
-                    String uuid = player.getUniqueId().toString();
-                    FancyMessage hList = new FancyMessage();
-                    hList.text(CustomMessages.getString("Info.homes"));
-                    try {
-                        if (Homes.getHomes(uuid).size()>0){
-                            for(String home : Homes.getHomes(uuid).keySet()){
-                                hList.then(home)
-                                        .command("/home " + home)
-                                        .tooltip(CustomMessages.getString("Tooltip.homes").replaceAll("\\{home}", home))
-                                        .then(", ");
-                            }
-                            hList.text(""); //Removes trailing comma
-                        } else {
-                            sender.sendMessage(CustomMessages.getString("Error.noHomes"));
-                            return true;
+                                return;
+                            } // Otherwise we'll just get the main player's homes
                         }
-
-                    } catch (NullPointerException ex) { // If a player has never set any homes
-                        sender.sendMessage(CustomMessages.getString("Error.noHomes"));
-                        return true;
                     }
-                    hList.send(player);
-                }
+                    if (sender instanceof Player) {
+                        Player player = (Player)sender;
+                        String uuid = player.getUniqueId().toString();
+                        FancyMessage hList = new FancyMessage();
+                        hList.text(CustomMessages.getString("Info.homes"));
+                        try {
+                            if (Homes.getHomes(uuid).size()>0){
+                                for(String home : Homes.getHomes(uuid).keySet()){
+                                    hList.then(home)
+                                            .command("/home " + home)
+                                            .tooltip(CustomMessages.getString("Tooltip.homes").replaceAll("\\{home}", home))
+                                            .then(", ");
+                                }
+                                if (player.getBedSpawnLocation() != null) {
+                                    hList.then("bed")
+                                            .command("/home bed")
+                                            .tooltip(CustomMessages.getString("Tooltip.homes").replaceAll("\\{home}", "bed"))
+                                            .then(", ");
+                                }
+                                hList.text(""); //Removes trailing comma
+                            } else {
+                                sender.sendMessage(CustomMessages.getString("Error.noHomes"));
+                                return;
+                            }
+
+                        } catch (NullPointerException ex) { // If a player has never set any homes
+                            sender.sendMessage(CustomMessages.getString("Error.noHomes"));
+                            return;
+                        }
+                        Bukkit.getScheduler().runTask(CoreClass.getInstance(), () -> hList.send(sender));
+                    }
+                });
+
             }
         } else {
             sender.sendMessage(CustomMessages.getString("Error.featureDisabled"));
