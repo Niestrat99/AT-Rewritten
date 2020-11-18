6d59e3f3140fc99a558620127ecb4b33d4ad3c5a
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-11-18 21:30:44 +0000

    Adding basic options

--- src/main/java/io/github/niestrat99/advancedteleport/config/NewConfig.java
+++ src/main/java/io/github/niestrat99/advancedteleport/config/NewConfig.java
@@ -6,13 +6,23 @@ public class NewConfig extends ConfigurationMaster{
      */
     public NewConfig() {
         super("config-new");
-        addDefault("use-basic-teleport-features", true, "Features", "Whether basic teleportation features should be enable or not." +
-                "\nThis includes /tpa, /tpahere, ");
+
     }
 
     @Override
     public void loadDefaults() {
+        addDefault("use-basic-teleport-features", true, "Features", "Whether basic teleportation features should be enable or not." +
+                "\nThis includes /tpa, /tpahere, /tpblock, /tpunblock and /back." +
+                "\nThis does not disable the command for other plugins - if you want other plugins to use the provided commands, use Bukkit's commands.yml file." +
+                "\nPlease refer to https://bukkit.gamepedia.com/Commands.yml for this!");
 
+        addDefault("use-warps", true, "Whether warps should be enabled in the plugin.");
+        addDefault("use-spawn", true, "Whether the plugin should modify spawn/spawn properties.");
+        addDefault("use-randomtp", true, "Whether the plugin should allow random teleportation.");
+        addDefault("use-homes", true, "Whether homes should be enabled in the plugin.");
+
+        addDefault("test.yes", true, "Test 1");
+        addDefault("test.yes-2", true, "Test 2");
     }
 
     @Override
