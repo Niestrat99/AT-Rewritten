e1d14d2297c56298b846f6312b7517671aceb50e
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-12-11 11:16:30 +0000

    Add anchor respawn option

--- src/main/java/io/github/niestrat99/advancedteleport/events/TeleportTrackingManager.java
+++ src/main/java/io/github/niestrat99/advancedteleport/events/TeleportTrackingManager.java
@@ -132,6 +132,9 @@ public class TeleportTrackingManager implements Listener {
                         e.setRespawnLocation(e.getPlayer().getBedSpawnLocation());
                     }
                     break;
+                case "anchor":
+                    // Vanilla just handles that
+                    break;
                 default:
                     if (spawnCommand.startsWith("warp:")) {
                         try {
