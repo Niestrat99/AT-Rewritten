1ae341327129484d5b739afbb904cc7cb7ff3f01
Thatsmusic99 <tmtherobotcat@gmail.com>
2021-01-10 10:16:20 +0000

    aaaahhh.

--- src/main/java/io/github/niestrat99/advancedteleport/sql/PlayerSQLManager.java
+++ src/main/java/io/github/niestrat99/advancedteleport/sql/PlayerSQLManager.java
@@ -3,8 +3,11 @@ package io.github.niestrat99.advancedteleport.sql;
 
 public class PlayerSQLManager extends SQLManager {
 
-    public PlayerSQLManager() {
+    private static PlayerSQLManager instance;
 
+    public PlayerSQLManager() {
+        super();
+        instance = this;
     }
 
     @Override
