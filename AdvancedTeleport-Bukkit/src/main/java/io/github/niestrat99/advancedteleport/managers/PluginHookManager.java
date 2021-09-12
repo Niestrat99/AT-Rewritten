d3f34ef292af150c00ed07801cb080da8f88f87f
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-09-12 14:33:17 +0100

    Add MapPlugin class

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/managers/PluginHookManager.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/managers/PluginHookManager.java
@@ -2,10 +2,12 @@ package io.github.niestrat99.advancedteleport.managers;
 
 import io.github.niestrat99.advancedteleport.hooks.BorderPlugin;
 import io.github.niestrat99.advancedteleport.hooks.ImportExportPlugin;
+import io.github.niestrat99.advancedteleport.hooks.MapPlugin;
 import io.github.niestrat99.advancedteleport.hooks.borders.ChunkyBorderHook;
 import io.github.niestrat99.advancedteleport.hooks.borders.VanillaBorderHook;
 import io.github.niestrat99.advancedteleport.hooks.borders.WorldBorderHook;
 import io.github.niestrat99.advancedteleport.hooks.imports.EssentialsHook;
+import io.github.niestrat99.advancedteleport.hooks.maps.Pl3xmapHook;
 import io.github.niestrat99.advancedteleport.utilities.RandomCoords;
 import org.bukkit.World;
 
@@ -15,6 +17,7 @@ public class PluginHookManager {
 
     private HashMap<String, ImportExportPlugin> importPlugins;
     private HashMap<String, BorderPlugin> borderPlugins;
+    private HashMap<String, MapPlugin> mapPlugins;
     private static PluginHookManager instance;
 
     public PluginHookManager() {
@@ -25,12 +28,19 @@ public class PluginHookManager {
     public void init() {
         importPlugins = new HashMap<>();
         borderPlugins = new HashMap<>();
+        mapPlugins = new HashMap<>();
 
         loadImportPlugin("essentials", EssentialsHook.class);
 
         loadBorderPlugin("worldborder", WorldBorderHook.class);
         loadBorderPlugin("chunkyborder", ChunkyBorderHook.class);
         loadBorderPlugin("vanilla", VanillaBorderHook.class);
+
+        loadMapPlugin("pl3xmap", Pl3xmapHook.class);
+
+        for (MapPlugin plugin : mapPlugins.values()) {
+            if (plugin.canEnable()) plugin.enable();
+        }
     }
 
     public static PluginHookManager get() {
@@ -58,6 +68,16 @@ public class PluginHookManager {
     private void loadBorderPlugin(String name, Class<? extends BorderPlugin> clazz) {
         try {
             borderPlugins.put(name, clazz.newInstance());
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoClassDefFoundError ignored) {
+
+        }
+    }
+
+    private void loadMapPlugin(String name, Class<? extends MapPlugin> clazz) {
+        try {
+            mapPlugins.put(name, clazz.newInstance());
         } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         } catch (NoClassDefFoundError ignored) {
