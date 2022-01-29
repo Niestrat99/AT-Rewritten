28b050a3b3b58938a4ec096af24a98af12241d20
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-01-29 11:19:37 +0000

    Initial commit :))

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/teleport/Tpa.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/teleport/Tpa.java
@@ -5,11 +5,8 @@ import io.github.niestrat99.advancedteleport.api.TeleportRequest;
 import io.github.niestrat99.advancedteleport.api.TeleportRequestType;
 import io.github.niestrat99.advancedteleport.api.events.players.TeleportRequestEvent;
 import io.github.niestrat99.advancedteleport.commands.TeleportATCommand;
-<<<<<<< HEAD
 import io.github.niestrat99.advancedteleport.api.ATFloodgatePlayer;
 import io.github.niestrat99.advancedteleport.api.ATPlayer;
-=======
->>>>>>> 42381dd (Improved API and more events (#78))
 import io.github.niestrat99.advancedteleport.config.CustomMessages;
 import io.github.niestrat99.advancedteleport.config.NewConfig;
 import io.github.niestrat99.advancedteleport.managers.CooldownManager;
@@ -30,7 +27,6 @@ public class Tpa extends TeleportATCommand {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!canProceed(sender)) return true;
-<<<<<<< HEAD
         if (!(sender instanceof Player)) {
             CustomMessages.sendMessage(sender, "Error.notAPlayer");
             return true;
@@ -108,81 +104,6 @@ public class Tpa extends TeleportATCommand {
            if (NewConfig.get().APPLY_COOLDOWN_AFTER.get().equalsIgnoreCase("request")) {
                CooldownManager.addToCooldown("tpa", player);
            }
-=======
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-
-            UUID playerUuid = player.getUniqueId();
-            int cooldown = CooldownManager.secondsLeftOnCooldown("tpa", player);
-            if (cooldown > 0) {
-                CustomMessages.sendMessage(sender, "Error.onCooldown", "{time}", String.valueOf(cooldown));
-                return true;
-            }
-            if (MovementManager.getMovement().containsKey(playerUuid)) {
-                CustomMessages.sendMessage(player, "Error.onCountdown");
-                return true;
-            }
-            if (args.length > 0) {
-                Player target = Bukkit.getPlayer(args[0]);
-                String result = ConditionChecker.canTeleport(player, target, "tpa");
-                if (result.isEmpty()) {
-                    assert target != null;
-                    if (PaymentManager.getInstance().canPay("tpa", player)) {
-
-                        TeleportRequestEvent event = new TeleportRequestEvent(target, player, TeleportRequestType.TPA);
-                        Bukkit.getPluginManager().callEvent(event);
-                        if (event.isCancelled()) {
-                            // Cannot send request
-                            return true;
-                        }
-
-                        int requestLifetime = NewConfig.get().REQUEST_LIFETIME.get();
-
-                        CustomMessages.sendMessage(sender, "Info.requestSent",
-                                "{player}", target.getName(),
-                                "{lifetime}", String.valueOf(requestLifetime));
-
-                        CoreClass.playSound("tpa", "sent", player);
-
-                        CustomMessages.sendMessage(target, "Info.tpaRequestReceived",
-                                "{player}", sender.getName(),
-                                "{lifetime}", String.valueOf(requestLifetime));
-
-                        CoreClass.playSound("tpa", "received", target);
-
-                        BukkitRunnable run = new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                if (NewConfig.get().NOTIFY_ON_EXPIRE.get()) {
-                                    CustomMessages.sendMessage(sender, "Error.requestExpired", "{player}",
-                                            target.getName());
-                                }
-                                TeleportRequest.removeRequest(TeleportRequest.getRequestByReqAndResponder(target,
-                                        player));
-                            }
-                        };
-                        run.runTaskLater(CoreClass.getInstance(), requestLifetime * 20L); // 60 seconds
-                        TeleportRequest request = new TeleportRequest(player, target, run, TeleportRequestType.TPA);
-                        // Creates a new teleport request.
-                        TeleportRequest.addRequest(request);
-                        // If the cooldown is to be applied after request, apply it now
-                        if (NewConfig.get().APPLY_COOLDOWN_AFTER.get().equalsIgnoreCase("request")) {
-                            CooldownManager.addToCooldown("tpa", player);
-                        }
-                        return true;
-                    }
-                } else {
-                    CustomMessages.sendMessage(player, result, "{player}", args[0], "{world}", target == null ? "<No " +
-                            "Such World>" : target.getWorld().getName());
-                    return true;
-                }
-            } else {
-                CustomMessages.sendMessage(sender, "Error.noPlayerInput");
-                return true;
-            }
-        } else {
-            CustomMessages.sendMessage(sender, "Error.notAPlayer");
->>>>>>> 42381dd (Improved API and more events (#78))
         }
         return true;
     }
