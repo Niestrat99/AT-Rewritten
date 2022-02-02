8577698a93226293aab65cdb931558ae2c6c1a0b
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-02-02 22:56:00 +0000

    Set up forms a bit more

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/teleport/Tpa.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/teleport/Tpa.java
@@ -40,72 +40,71 @@ public class Tpa extends TeleportATCommand {
                 CustomMessages.sendMessage(player, "Error.onCountdown");
                 return true;
             }
-            if (args.length > 0) {
-                Player target = Bukkit.getPlayer(args[0]);
-                String result = ConditionChecker.canTeleport(player, target, "tpa");
-                if (result.isEmpty()) {
-                    assert target != null;
-                    if (PaymentManager.getInstance().canPay("tpa", player)) {
-
-                        TeleportRequestEvent event = new TeleportRequestEvent(target, player, TeleportRequestType.TPA);
-                        Bukkit.getPluginManager().callEvent(event);
-                        if (event.isCancelled()) {
-                            // Cannot send request
-                            return true;
-                        }
-
-                        int requestLifetime = NewConfig.get().REQUEST_LIFETIME.get();
-
-                        CustomMessages.sendMessage(sender, "Info.requestSent",
-                                "{player}", target.getName(),
-                                "{lifetime}", String.valueOf(requestLifetime));
-
-                        CoreClass.playSound("tpa", "sent", player);
-
-                        ATPlayer targetPlayer = ATPlayer.getPlayer(target);
-
-                        if (targetPlayer instanceof ATFloodgatePlayer) {
-                            ((ATFloodgatePlayer) targetPlayer).sendRequestFormTPA(player);
-                        } else {
-                            CustomMessages.sendMessage(target, "Info.tpaRequestReceived",
-                                    "{player}", sender.getName(),
-                                    "{lifetime}", String.valueOf(requestLifetime));
-                        }
-
-                        CoreClass.playSound("tpa", "received", target);
-
-                        BukkitRunnable run = new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                if (NewConfig.get().NOTIFY_ON_EXPIRE.get()) {
-                                    CustomMessages.sendMessage(sender, "Error.requestExpired", "{player}",
-                                            target.getName());
-                                }
-                                TeleportRequest.removeRequest(TeleportRequest.getRequestByReqAndResponder(target,
-                                        player));
-                            }
-                        };
-                        run.runTaskLater(CoreClass.getInstance(), requestLifetime * 20L); // 60 seconds
-                        TeleportRequest request = new TeleportRequest(player, target, run, TeleportRequestType.TPA);
-                        // Creates a new teleport request.
-                        TeleportRequest.addRequest(request);
-                        // If the cooldown is to be applied after request, apply it now
-                        if (NewConfig.get().APPLY_COOLDOWN_AFTER.get().equalsIgnoreCase("request")) {
-                            CooldownManager.addToCooldown("tpa", player);
-                        }
-                        return true;
-                    }
+            if (args.length == 0) {
+                ATPlayer atPlayer = ATPlayer.getPlayer(player);
+                if (atPlayer instanceof ATFloodgatePlayer) {
+                    ((ATFloodgatePlayer) atPlayer).sendTPAForm();
                 } else {
-                    CustomMessages.sendMessage(player, result, "{player}", args[0], "{world}", target == null ? "<No " +
-                            "Such World>" : target.getWorld().getName());
-                    return true;
+                    CustomMessages.sendMessage(sender, "Error.noPlayerInput");
                 }
-            } else {
-                CustomMessages.sendMessage(sender, "Error.noPlayerInput");
                 return true;
             }
-        } else {
-            CustomMessages.sendMessage(sender, "Error.notAPlayer");
+            Player target = Bukkit.getPlayer(args[0]);
+            String result = ConditionChecker.canTeleport(player, target, "tpa");
+            if (!result.isEmpty()) {
+                CustomMessages.sendMessage(player, result, "{player}", args[0], "{world}", target == null ? "<No Such World>" : target.getWorld().getName());
+                return true;
+            }
+            if (PaymentManager.getInstance().canPay("tpa", player)) {
+
+                TeleportRequestEvent event = new TeleportRequestEvent(target, player, TeleportRequestType.TPA);
+                Bukkit.getPluginManager().callEvent(event);
+                if (event.isCancelled()) {
+                    // Cannot send request
+                    return true;
+                }
+
+                int requestLifetime = NewConfig.get().REQUEST_LIFETIME.get();
+
+                CustomMessages.sendMessage(sender, "Info.requestSent",
+                        "{player}", target.getName(),
+                        "{lifetime}", String.valueOf(requestLifetime));
+
+                CoreClass.playSound("tpa", "sent", player);
+
+                ATPlayer targetPlayer = ATPlayer.getPlayer(target);
+
+                if (targetPlayer instanceof ATFloodgatePlayer) {
+                    ((ATFloodgatePlayer) targetPlayer).sendRequestFormTPA(player);
+                } else {
+                    CustomMessages.sendMessage(target, "Info.tpaRequestReceived",
+                            "{player}", sender.getName(),
+                            "{lifetime}", String.valueOf(requestLifetime));
+                }
+
+                CoreClass.playSound("tpa", "received", target);
+
+                BukkitRunnable run = new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        if (NewConfig.get().NOTIFY_ON_EXPIRE.get()) {
+                            CustomMessages.sendMessage(sender, "Error.requestExpired", "{player}",
+                                    target.getName());
+                        }
+                        TeleportRequest.removeRequest(TeleportRequest.getRequestByReqAndResponder(target,
+                                player));
+                    }
+                };
+                run.runTaskLater(CoreClass.getInstance(), requestLifetime * 20L); // 60 seconds
+                TeleportRequest request = new TeleportRequest(player, target, run, TeleportRequestType.TPA);
+                // Creates a new teleport request.
+                TeleportRequest.addRequest(request);
+                // If the cooldown is to be applied after request, apply it now
+                if (NewConfig.get().APPLY_COOLDOWN_AFTER.get().equalsIgnoreCase("request")) {
+                    CooldownManager.addToCooldown("tpa", player);
+                }
+                return true;
+            }
         }
         return true;
     }
