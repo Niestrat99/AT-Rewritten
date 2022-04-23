3d4f356d5aa17e6566ce1d4cd5cce050c3150f0f
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-04-23 11:50:43 +0100

    Document ToggleTeleportationEvent

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/api/events/players/ToggleTeleportationEvent.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/api/events/players/ToggleTeleportationEvent.java
@@ -7,10 +7,14 @@ import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * The event fired when a player's teleportation status has been changed.
+ */
 public class ToggleTeleportationEvent extends TrackableATEvent {
 
+    private static final HandlerList handlers = new HandlerList();
     @NotNull
-    private OfflinePlayer player;
+    private final OfflinePlayer player;
     private boolean enabled;
     private boolean statusChanging;
 
@@ -21,9 +25,51 @@ public class ToggleTeleportationEvent extends TrackableATEvent {
         this.statusChanging = statusChanging;
     }
 
+    /**
+     * Returns the player who is having their teleportation status changed.
+     *
+     * @return the player that is having their teleportation status changed.
+     */
+    @NotNull
+    public OfflinePlayer getPlayer() {
+        return player;
+    }
+
+    /**
+     * Returns the status that the player's teleportation status is being set to.
+     *
+     * @return true if the player's teleportation is going to be enabled, false if it is being disabled.
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Sets the status of the player's teleportation after the event, as long as it is uncancelled.
+     *
+     * @param enabled true to enable the player teleportation, false to disable it.
+     */
+    public void setEnabled(boolean enabled) {
+        if (this.enabled != enabled) statusChanging = !statusChanging;
+        this.enabled = enabled;
+    }
+
+    /**
+     * Returns whether the player's teleportation status is changing from what it currently is.
+     *
+     * @return true if the status is changing (i.e. true to false, false to true) or false if it is not.
+     */
+    public boolean isStatusChanging() {
+        return statusChanging;
+    }
+
     @NotNull
     @Override
     public HandlerList getHandlers() {
-        return null;
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
