0363570a4b5ebc87e89fcbde274f482d578c8174
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-07-23 17:57:26 +0100

    Add map commands

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/core/MapCommand.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/core/MapCommand.java
@@ -1,17 +1,27 @@
 package io.github.niestrat99.advancedteleport.commands.core;
 
 import io.github.niestrat99.advancedteleport.commands.SubATCommand;
+import io.github.niestrat99.advancedteleport.commands.core.map.SetIconCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MapCommand implements SubATCommand {
+
+    private final HashMap<String, SubATCommand> subMapCommands;
+
+    public MapCommand() {
+        subMapCommands = new HashMap<>();
+        subMapCommands.put("seticon", new SetIconCommand());
+    }
+
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s,
-                             @NotNull String[] strings) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String s,
+                             @NotNull String[] args) {
         return false;
     }
 
