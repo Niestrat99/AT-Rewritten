207d5c99ee83b866bf8ff3d67a523f1aef890eab
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-07-23 17:52:56 +0100

    Add more map hook functions

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/hooks/MapPlugin.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/hooks/MapPlugin.java
@@ -18,6 +18,18 @@ public abstract class MapPlugin {
 
     public abstract void addSpawn(String name, Location location);
 
+    public abstract void removeWarp(Warp warp);
+
+    public abstract void removeHome(Home home);
+
+    public abstract void removeSpawn(String name);
+
+    public abstract void moveWarp(Warp warp);
+
+    public abstract void moveHome(Home home);
+
+    public abstract void moveSpawn(String name, Location location);
+
     public abstract void registerImage(String name, InputStream stream);
 
     public enum TeleportPoint {
