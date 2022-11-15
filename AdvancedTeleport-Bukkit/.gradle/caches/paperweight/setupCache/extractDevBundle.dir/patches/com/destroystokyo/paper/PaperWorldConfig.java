0d485c81b65db1d2e44c6dd790420008f53e0d13
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2022-11-15 11:38:03 +0000

    I SAID NUKE

--- AdvancedTeleport-Bukkit/.gradle/caches/paperweight/setupCache/extractDevBundle.dir/patches/com/destroystokyo/paper/PaperWorldConfig.java
+++
@@ -1,8 +0,0 @@
-package com.destroystokyo.paper;
-
-/**
- * @deprecated kept as a means to identify Paper in older plugins/PaperLib
- */
-@Deprecated(forRemoval = true)
-public class PaperWorldConfig {
-}
