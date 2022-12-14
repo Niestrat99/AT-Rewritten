251a7ad9d7d30425695e54d3c33c714309034472
DaRacci <racci@racci.dev>
2022-12-15 00:28:22 +1100

    fix: ClassCastException

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/api/ATPlayer.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/api/ATPlayer.java
@@ -15,11 +15,11 @@ import io.github.niestrat99.advancedteleport.sql.BlocklistManager;
 import io.github.niestrat99.advancedteleport.sql.HomeSQLManager;
 import io.github.niestrat99.advancedteleport.sql.PlayerSQLManager;
 import io.github.niestrat99.advancedteleport.sql.SQLManager;
+import io.github.thatsmusic99.configurationmaster.impl.CMConfigSection;
 import io.papermc.lib.PaperLib;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
@@ -700,12 +700,12 @@ public class ATPlayer {
     }
 
     @Range(from = 0, to = Integer.MAX_VALUE)
-    private int getMin(String permission, String command, ConfigurationSection customSection, int defaultValue) {
+    private int getMin(String permission, String command, CMConfigSection customSection, int defaultValue) {
         return determineValue(permission, command, defaultValue, customSection, Math::min);
     }
 
     @Range(from = 0, to = Integer.MAX_VALUE)
-    private int determineValue(String permission, String command, int defaultValue, ConfigurationSection customSection, BiFunction<Integer, Integer, Integer> consumer) {
+    private int determineValue(String permission, String command, int defaultValue, CMConfigSection customSection, BiFunction<Integer, Integer, Integer> consumer) {
         List<String> cooldowns = new ArrayList<>();
 
         // If the player is null, return the default value
