e116ba3248fcbdd52cfaad003852d01bb0928a89
DaRacci <racci@racci.dev>
2022-12-15 00:26:12 +1100

    refactor(gradle): add minix catalog for libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,18 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.racci.dev/releases") { mavenContent { releasesOnly() } }
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories.maven("https://repo.racci.dev/releases") { mavenContent { releasesOnly() } }
+
+    versionCatalogs.create("libMinix") {
+        val minixVersion: String by settings
+        val kotlinVersion: String by settings
+        val conventions = kotlinVersion.plus("-").plus(minixVersion.substringAfterLast('.'))
+        from("dev.racci:catalog:$conventions")
     }
 }
