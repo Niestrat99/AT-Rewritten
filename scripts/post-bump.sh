68c0d47750bc3e18ef965a942a26c9c026c06a3f
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2022-12-26 12:31:04 +0000

    dev: good god

--- scripts/post-bump.sh
+++ scripts/post-bump.sh
@@ -2,4 +2,5 @@ echo -e \"group=io.github.niestrat99\nversion=$1\" > gradle.properties
 git add gradle.properties
 git commit -m "build: Update version to v$1"
 git tag -a v$1 -m "Version v$1"
+git push
 git push origin v$1
