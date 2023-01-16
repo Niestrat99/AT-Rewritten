9803fed149ac13442192a3659d85d57b3af0494d
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-01-16 14:35:33 +0000

    chore: separate signs into their own classes

---
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/api/signs/BedSign.java
@@ -0,0 +1,28 @@
+package io.github.niestrat99.advancedteleport.api.signs;
+
+import io.github.niestrat99.advancedteleport.api.ATPlayer;
+import io.github.niestrat99.advancedteleport.api.ATSign;
+import io.github.niestrat99.advancedteleport.commands.home.HomeCommand;
+import io.github.niestrat99.advancedteleport.config.NewConfig;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class BedSign extends ATSign {
+
+    public BedSign() {
+        super("Bed", NewConfig.get().USE_HOMES.get());
+    }
+
+    @Override
+    public void onInteract(@NotNull Sign sign, @NotNull Player player) {
+        ATPlayer atPlayer = ATPlayer.getPlayer(player);
+        if (atPlayer.getBedSpawn() == null) return;
+        HomeCommand.teleport(player, atPlayer.getBedSpawn());
+    }
+
+    @Override
+    public boolean canCreate(@NotNull Sign sign, @NotNull Player player) {
+        return true;
+    }
+}
---
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/api/signs/HomeSign.java
@@ -0,0 +1,35 @@
+package io.github.niestrat99.advancedteleport.api.signs;
+
+import io.github.niestrat99.advancedteleport.api.ATPlayer;
+import io.github.niestrat99.advancedteleport.api.ATSign;
+import io.github.niestrat99.advancedteleport.api.Home;
+import io.github.niestrat99.advancedteleport.commands.home.HomeCommand;
+import io.github.niestrat99.advancedteleport.config.NewConfig;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class HomeSign extends ATSign {
+
+    public HomeSign() {
+        super("Home", NewConfig.get().USE_HOMES.get());
+    }
+
+    @Override
+    public void onInteract(@NotNull Sign sign, @NotNull Player player) {
+        ATPlayer atPlayer = ATPlayer.getPlayer(player);
+
+        Home mainHome = atPlayer.getMainHome();
+        if (mainHome == null) {
+            if (atPlayer.getHomes().size() == 0) return;
+            mainHome = atPlayer.getHomes().values().iterator().next();
+        }
+
+        HomeCommand.teleport(player, mainHome);
+    }
+
+    @Override
+    public boolean canCreate(@NotNull Sign sign, @NotNull Player player) {
+        return true;
+    }
+}
