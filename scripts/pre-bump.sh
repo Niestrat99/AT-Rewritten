3ec1159eb1eb2c9d3bb44bfff071fafef7fcfa73
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-01-17 14:24:16 +0000

    refractor: Update pre-bump scripts for Cog to support flexible properties layout

--- scripts/pre-bump.sh
+++ scripts/pre-bump.sh
@@ -1,2 +1,8 @@
-echo -e "group=io.github.niestrat99\nversion=$1" > gradle.properties # TODO - update
-git add gradle.properties
+
+# Get the current layout of the file
+PROPERTIES_CONTENT=$(cat gradle.properties)
+
+# Replace the current version with the new version, and write to the file again
+echo "${PROPERTIES_CONTENT/$1/$2}" > gradle.properties
+
+
