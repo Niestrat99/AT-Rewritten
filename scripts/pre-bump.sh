34e7d5258e8ea36221f78baa86eeed4ae795d9b6
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-04-10 17:17:19 +0100

    dev: make internal tweaks

--- scripts/pre-bump.sh
+++ scripts/pre-bump.sh
@@ -5,4 +5,5 @@ PROPERTIES_CONTENT=$(cat gradle.properties)
 # Replace the current version with the new version, and write to the file again
 echo "${PROPERTIES_CONTENT/$1/$2}" > gradle.properties
 
+exit(0)
 
