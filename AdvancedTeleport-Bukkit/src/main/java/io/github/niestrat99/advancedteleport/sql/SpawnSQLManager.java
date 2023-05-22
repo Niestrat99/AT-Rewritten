40954b1e4f4216ae7b389cb684dd856b4602278b
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2023-05-22 11:26:43 +0100

    feat: initial work for Folia support

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/sql/SpawnSQLManager.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/sql/SpawnSQLManager.java
@@ -4,7 +4,6 @@ import io.github.niestrat99.advancedteleport.CoreClass;
 import io.github.niestrat99.advancedteleport.api.data.UnloadedWorldException;
 import io.github.niestrat99.advancedteleport.folia.RunnableManager;
 import io.github.niestrat99.advancedteleport.api.Spawn;
-import io.github.niestrat99.advancedteleport.folia.RunnableManager;
 import io.github.niestrat99.advancedteleport.managers.NamedLocationManager;
 
 import org.bukkit.Location;
