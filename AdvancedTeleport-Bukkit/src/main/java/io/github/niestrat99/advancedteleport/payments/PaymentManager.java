cb287f494b21d666074a8b439e7f231a480c5815
Holly P <25277367+Thatsmusic99@users.noreply.github.com>
2023-07-25 13:10:59 +0100

    fix: item payments being parsed incorrectly

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/payments/PaymentManager.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/payments/PaymentManager.java
@@ -102,9 +102,6 @@ public class PaymentManager {
                 CoreClass.debug("Split payment into two, part 1: " + plugin + ", part two: " + payment);
                 CoreClass.debug("Material: " + Material.getMaterial(plugin == null ? "" : plugin));
 
-                CoreClass.debug("Split payment into two, part 1: " + plugin + ", part two: " + payment);
-                CoreClass.debug("Material: " + Material.getMaterial(plugin == null ? "" : plugin));
-
                 // If it's not a plugin and actually an item, lmao
                 if (plugin != null && Material.getMaterial(plugin) != null) {
                     return ItemsPayment.getFromString(rawPayment);
