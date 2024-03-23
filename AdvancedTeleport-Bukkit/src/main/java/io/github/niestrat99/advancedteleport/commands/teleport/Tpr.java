02a16b98f5fe371f9a73c3cbdc59731dcccf1dcc
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2024-03-23 10:59:04 +0000

    fix: /rtp trying to call teleport event async
    Signed-off-by: Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/teleport/Tpr.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/commands/teleport/Tpr.java
@@ -1,5 +1,6 @@
 package io.github.niestrat99.advancedteleport.commands.teleport;
 
+import io.github.niestrat99.advancedteleport.CoreClass;
 import io.github.niestrat99.advancedteleport.api.ATPlayer;
 import io.github.niestrat99.advancedteleport.api.AdvancedTeleportAPI;
 import io.github.niestrat99.advancedteleport.api.events.ATTeleportEvent;
@@ -127,7 +128,7 @@ public final class Tpr extends ATCommand implements TimedATCommand {
 
         // Search for a random location
         AdvancedTeleportAPI.getRandomLocation(world, player)
-                .whenComplete(
+                .whenCompleteAsync(
                         (result, err) -> {
 
                             // If there was an error, let the player know
@@ -139,7 +140,7 @@ public final class Tpr extends ATCommand implements TimedATCommand {
 
                             // Process the teleportation location
                             processLocation(player, result);
-                        });
+                        }, CoreClass.sync);
 
         return true;
     }
