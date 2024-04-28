d4ea1c56a932a76feace7d8930fb1966d9f01a71
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2024-04-29 00:43:56 +0100

    fix: spark not working without hooked plugins
    Signed-off-by: Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>

--- AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/managers/ParticleManager.java
+++ AdvancedTeleport-Bukkit/src/main/java/io/github/niestrat99/advancedteleport/managers/ParticleManager.java
@@ -24,25 +24,32 @@ public final class ParticleManager {
     public static void removeParticles(
             @NotNull final Player player, @NotNull final String command) {
         if (!MainConfig.get().USE_PARTICLES.get()) return;
+
+        // Check if using inbuilt particles
+        final var particle = MainConfig.get().TELEPORT_PARTICLES.valueOf(command).get();
+        if (particle.equals("spark")) {
+            doSpark(player.getLocation());
+            return;
+        }
+
         PluginHookManager.get()
                 .getPluginHooks(ParticlesPlugin.class, true)
-                .map(
-                        hook -> {
-                            hook.removeParticles(player, command);
-                            return MainConfig.get().TELEPORT_PARTICLES.valueOf(command).get();
-                        })
-                .filter("spark"::equals)
-                .forEach(inbuilt -> doSpark(player.getLocation()));
+                .forEach(hook -> hook.removeParticles(player, command));
     }
 
     public static void onTeleport(@NotNull final Player player, @NotNull final String command) {
         if (!MainConfig.get().USE_PARTICLES.get()) return;
         removeParticles(player, command);
-        PluginHookManager.get()
-                .getPluginHooks(ParticlesPlugin.class, true)
-                .map(hook -> MainConfig.get().TELEPORT_PARTICLES.valueOf(command).get())
-                .filter("spark"::equals)
-                .forEach(hook -> doSpark(player.getLocation()));
+
+        // Check if using inbuilt particles
+        final var particle = MainConfig.get().TELEPORT_PARTICLES.valueOf(command).get();
+        if (particle.equals("spark")) {
+            doSpark(player.getLocation());
+            return;
+        }
+
+        applyParticles(player, command);
+        Bukkit.getServer().getScheduler().runTask(CoreClass.getInstance(), () -> removeParticles(player, command));
     }
 
     public static @Nullable String getData(@NotNull final Player player) {
