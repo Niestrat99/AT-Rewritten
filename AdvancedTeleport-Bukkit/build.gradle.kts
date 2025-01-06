67b2c53e9fb8c10bed2273820c26d1876512b383
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2025-01-06 22:11:24 +0000

    chore: add supported versions
    Signed-off-by: Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>

--- AdvancedTeleport-Bukkit/build.gradle.kts
+++ AdvancedTeleport-Bukkit/build.gradle.kts
@@ -198,7 +198,7 @@ modrinth {
     versionType.set(getReleaseType())
     uploadFile.set(tasks.shadowJar.get())
     gameVersions.addAll(arrayListOf("1.18", "1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20",
-        "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6"))
+        "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4"))
     loaders.addAll("paper", "spigot", "purpur")
     changelog.set(getCogChangelog())
 }
@@ -215,7 +215,7 @@ hangarPublish {
             register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
                 jar.set(getJarFile())
                 platformVersions.set(listOf("1.18", "1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4",
-                    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6"))
+                    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4"))
                 dependencies {
                     url("Vault", "https://dev.bukkit.org/projects/vault") {
                         required.set(false)
