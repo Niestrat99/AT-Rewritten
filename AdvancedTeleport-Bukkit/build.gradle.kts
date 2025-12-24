b585d81143c224f17bcc52b66041bcdfb59f93f1
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2025-12-24 15:40:49 +0100

    chore(version): bump to 6.2.0

--- AdvancedTeleport-Bukkit/build.gradle.kts
+++ AdvancedTeleport-Bukkit/build.gradle.kts
@@ -215,7 +215,8 @@ hangarPublish {
             register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
                 jar.set(getJarFile())
                 platformVersions.set(listOf("1.18", "1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4",
-                    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4"))
+                    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4",
+                    "1.21.11"))
                 dependencies {
                     url("Vault", "https://dev.bukkit.org/projects/vault") {
                         required.set(false)
